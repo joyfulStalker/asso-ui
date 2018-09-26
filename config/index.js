16172d38709404bad892833a5c993c112aedf891
lsl <liuslc@yonyou.com>
2018-09-26 17:33:58 +0800

    asso-ui首次提交

---
+++ config/index.js
@@ -0,0 +1,41 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8082,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/mypath': {
+        target: 'http://localhost:8090',//设置你调用的接口域名和端口号 别忘了加http
+        changeOrigin: true,
+        pathRewrite: {
+            '^/mypath': '/' // 这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+        }
+      }
+    },
+    cssSourceMap: false
+  }
+}
---
+++ src/components/errColl/errManage.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="app-container">
+    <div class="filter-container" style="margin:10px 0">
+      <el-form :model="queryParm" label-width="80px">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-form-item label="错误描述">
+              <el-input v-model="queryParm.errDescription"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="错误原因">
+              <el-input v-model="queryParm.errCauseBy"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="解决方法">
+              <el-input v-model="queryParm.errSolution"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row style="text-align:right;">
+          <el-button type="primary" @click="getErrList">查询</el-button>
+          <el-button type="primary" @click="clear">清空查询</el-button>
+        </el-row>
+      </el-form>
+    </div>
+
+    <div style="margin:10px 0">
+      <el-table :data="errList" border style="width: 100%">
+          <el-table-column align="center" label="序号" width="65"  type="index"> </el-table-column>
+          <el-table-column align="center" prop="errDescription" label="错误描述" width="340"></el-table-column>
+          <el-table-column align="center" prop="errCauseBy" label="错误原因" width="340"></el-table-column>
+          <el-table-column align="center" prop="errSolution" label="解决方法" width="340"></el-table-column>
+          <el-table-column align="center" prop="solveTime" label="解决时间" width="180"></el-table-column>
+      </el-table>
+    </div>
+    <div v-show="!listLoading" class="pagination-container">
+      <el-pagination @size-change="handleSizeChange" background @current-change="handleCurrentChange" :current-page.sync="queryParm.page" :page-sizes="[10,20,30, 50]" :page-size="queryParm.rows" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
+    </div>
+  </div>
+</template>
+<script>
+import request from '@/utils/request';
+export default {
+  name: "errManage",
+  data() {
+    return {
+        errList:[],
+        listLoading: false,
+        total: null,
+        queryParm: {
+          errDescription: '',
+          errCauseBy: '',
+          errSolution: '',
+          page: 1,
+          rows: 10
+        }
+    }
+  },
+  created() {
+    this.getErrList();
+  },
+  mounted(){
+
+  },
+  methods: {
+    getErrList() {
+      request({
+        url: '/errCollection/errList',
+        method: 'post',
+        data: this.queryParm
+      }).then(result => {
+        this.errList = result.data.data;
+        this.total = result.data.total;
+      })
+    },
+    clear() {
+      this.queryParm = {
+        errDescription: '',
+        errCauseBy: '',
+        errSolution: '',
+        page: 1,
+        rows: 10
+      }
+    },
+    handleSizeChange(val) {
+      this.queryParm.rows = val;
+      this.getErrList();
+    },
+    handleCurrentChange(val) {
+      this.queryParm.page = val;
+      this.getErrList();
+    },
+  }
+}
+</script>
