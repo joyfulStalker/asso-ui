30de596d78c2b386c4805bbe446f021137166d73
lsl <liuslc@yonyou.com>
2018-11-20 12:50:29 +0800

    密码更改页面

--- src/components/user/changepwd.vue
+++ src/components/user/changepwd.vue
@@ -8,16 +8,16 @@
       </el-breadcrumb>
     </el-col>
 
-    <el-col :span="24" class="warp-main">
+    <el-col :span="24" class="warp-main demo-ruleForm">
       <el-form ref="form" :model="form" label-width="120px">
         <el-form-item label="原密码">
-          <el-input v-model="form.oldPwd"></el-input>
+          <el-input type="password" v-model="form.oldPassword" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="新密码">
-          <el-input v-model="form.newPwd"></el-input>
+          <el-input type="password" v-model="form.newPassword" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="确认新密码">
-          <el-input v-model="form.confirmPwd"></el-input>
+          <el-input type="password" v-model="form.confirmPwd" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="default" @click="handleChangepwd">提交</el-button>
@@ -27,20 +27,53 @@
   </el-row>
 </template>
 <script>
-  export default{
-    data(){
-      return {
-        form: {
-          oldPwd: '',
-          newPwd: '',
-          confirmPwd: ''
-        }
-      }
-    },
-    methods: {
-      handleChangepwd() {
-        this.$message({message: "此功能只是让你看看，不会开发！", duration: 2000});
+import request from "@/utils/request";
+export default {
+  data() {
+    return {
+      form: {
+        oldPassword: "",
+        newPassword: "",
+        confirmPwd: ""
       }
     }
+  },
+  methods: {
+    handleClear(){
+      this.form = {
+        oldPassword: "",
+        newPassword: "",
+        confirmPwd: ""
+      }
+    },
+    handleChangepwd() {
+      //判断newPassword和confirmPwd是否一致
+      if(this.form.newPassword != this.form.confirmPwd){
+        this.$message.error("新密码和确认密码不一致!");
+      }
+      request({
+        url: "/userAccount/changePwd",
+        method: "post",
+        data: this.form
+      }).then(result => {
+          if (result.data.resultCode == 200) {
+            this.$message({
+              message: "恭喜你，密码修改成功！",
+              type: "success"
+            });
+          } else {
+            this.$message.error(result.data.errMsg);
+          }
+        }).catch(function(error) {
+          this.loading = false;
+          console.log(error);
+          this.$message.error({
+            showClose: true,
+            message: "请求出现异常",
+            duration: 2000
+          });
+        });
+    }
   }
+};
 </script>
