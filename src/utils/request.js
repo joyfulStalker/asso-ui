1c081bbbd72ce266dbead645c3d233195e6184de
joyfulStalker <joyfulStalker@qq.com>
2019-04-26 14:53:12 +0800

    请求头携带token信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 // 创建axios实例
 const service = axios.create({
   baseURL: '/mypath', // api的base_url
-  timeout: 5000 // 请求超时时间
+  timeout: 5000,// 请求超时时间
+  headers: {
+    'Authorization': window.localStorage.getItem('access-user') == null ? '' : JSON.parse(window.localStorage.getItem('access-user')).token
+  }
 });
 
 export default service;
